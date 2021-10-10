06083fdefe9b6899ec04e73ff30294cd1e4beb30
Anas Abu Farraj <anas.r.abufarraj@gmail.com>
2021-10-10 18:42:37 +0300

    Tags empty state

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -11,13 +11,16 @@ class Counter extends Component {
       <React.Fragment>
         <span className={`badge m-2 ${this.setBadgeColor()}`}>{this.setCounterText()}</span>
         <button className="btn btn-secondary btn-sm">Increment</button>
-        <ul>
-          {this.state.tags.map((tag) => (
-            <li key={this.randomId()}>{tag}</li>
-          ))}
-        </ul>
+        <ul>{this.emptyState()}</ul>
       </React.Fragment>
     );
+  }
+
+  emptyState() {
+    if (this.state.tags.length) {
+      return this.state.tags.map((tag) => <li key={this.randomId()}>{tag}</li>);
+    }
+    return <p>You have no tags!</p>;
   }
 
   randomId() {
