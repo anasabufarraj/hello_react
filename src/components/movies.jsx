e1e5ee7ee6ebc34e091d98d11bc58e3e74490012
Anas Abu Farraj <anas.r.abufarraj@gmail.com>
2021-10-12 00:20:57 +0300

    My solution to exercise 37

---
+++ src/components/movies.jsx
@@ -0,0 +1,62 @@
+import React, { Component } from 'react';
+import { getMovies, deleteMovie } from '../services/fakeMovieService';
+
+class Movies extends Component {
+  state = {
+    movies: getMovies(),
+  };
+
+  render() {
+    return (
+      <div className="container">
+        <p className="m-2 my-2">{this.emptyState()}...</p>
+        <table className="table table-hover">
+          <thead>
+            <tr>
+              <th scope="col">Movie</th>
+              <th scope="col">Genre</th>
+              <th scope="col">In Stock</th>
+              <th scope="col">Handle</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.movies.map((movie) => (
+              <tr key={this.randomId()}>
+                <td key={this.randomId()}>{movie.title}</td>
+                <td key={this.randomId()}>{movie.genre.name}</td>
+                <td key={this.randomId()}>{movie.numberInStock}</td>
+                <td key={this.randomId()}>
+                  <button
+                    onClick={() => this.updateTable(movie._id)}
+                    className="btn btn-danger btn-sm"
+                  >
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    );
+  }
+
+  emptyState() {
+    if (this.state.movies.length === 0) {
+      return `You've no movies to show`;
+    }
+    return this.state.movies.length > 1
+      ? `Showing ${this.state.movies.length} Movies`
+      : `Showing ${this.state.movies.length} Movie`;
+  }
+
+  updateTable(id) {
+    this.setState(deleteMovie(id));
+  }
+
+  randomId() {
+    return Math.random();
+  }
+}
+
+export default Movies;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-// import App from './App';
-import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'font-awesome/css/font-awesome.css';
-import Counter from './components/counter';
+import Movies from './components/movies';
+import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(<Counter />, document.getElementById('root'));
+ReactDOM.render(
+  <React.StrictMode>
+    <Movies />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
 reportWebVitals();
