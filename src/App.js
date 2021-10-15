c2fbabc5013cd9b2960c88f1d536b632f11c2fe5
Anas Abu Farraj <anas.r.abufarraj@gmail.com>
2021-10-15 14:12:40 +0300

    Lifting State Up

--- src/App.js
+++ src/App.js
@@ -4,12 +4,61 @@ import Counters from './components/counters';
 import './App.css';
 
 class App extends Component {
+  state = {
+    counters: [
+      { id: 1, value: 0 },
+      { id: 2, value: 0 },
+      { id: 3, value: 0 },
+      { id: 4, value: 0 },
+      { id: 5, value: 0 },
+    ],
+  };
+
+  constructor(props) {
+    super(props);
+    this.handleReset = this.handleReset.bind(this);
+    this.handleIncrement = this.handleIncrement.bind(this);
+    this.handleDelete = this.handleDelete.bind(this);
+  }
+
+  handleReset() {
+    let counters = this.state.counters.map((_c) => {
+      _c.value = 0;
+      return _c;
+    });
+    this.setState({ counters });
+  }
+
+  handleIncrement(counter) {
+    let counters = [...this.state.counters];
+    let index = counters.indexOf(counter);
+    counters[index].value++;
+    this.setState({ counters });
+  }
+
+  handleDelete(id) {
+    let counters = this.state.counters.filter((_c) => _c.id !== id);
+    this.setState({ counters });
+  }
+
+  handleEmptyState() {
+    if (this.state.counters.length === 0) {
+      return <p className="text-secondary mx-4">You have no counters to view</p>;
+    }
+  }
+
   render() {
     return (
       <React.Fragment>
         <NavBar />
         <main className="container">
-          <Counters />
+          <Counters
+            onReset={this.handleReset}
+            onIncrement={this.handleIncrement}
+            onDelete={this.handleDelete}
+            counters={this.state.counters}
+          />
+          {this.handleEmptyState()}
         </main>
       </React.Fragment>
     );
--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -2,55 +2,17 @@ import React, { Component } from 'react';
 import Counter from './counter';
 
 class Counters extends Component {
-  state = {
-    counters: [
-      { id: 1, value: 0 },
-      { id: 2, value: 0 },
-      { id: 3, value: 0 },
-      { id: 4, value: 0 },
-      { id: 5, value: 0 },
-    ],
-  };
-
-  constructor(props) {
-    super(props);
-    this.handleReset = this.handleReset.bind(this);
-  }
-
-  handleReset() {
-    let counters = this.state.counters.map((_c) => {
-      _c.value = 0;
-      return _c;
-    });
-    this.setState({ counters });
-  }
-
-  handleIncrement(counter) {
-    let counters = [...this.state.counters];
-    let index = counters.indexOf(counter);
-    counters[index].value++;
-    this.setState({ counters });
-  }
-
-  handleDelete(id) {
-    let counters = this.state.counters.filter((_c) => _c.id !== id);
-    this.setState({ counters });
-  }
-
   render() {
-    // if (this.state.counters.length === 0) {
-    //   return <div className="text-secondary m-4">You have no counters!</div>;
-    // }
     return (
       <React.Fragment>
-        <button onClick={this.handleReset} className="btn btn-outline-danger m-4">
+        <button onClick={this.props.onReset} className="btn btn-outline-danger m-4">
           Reset Quantity
         </button>
-        {this.state.counters.map((counter) => (
+        {this.props.counters.map((counter) => (
           <Counter
             key={counter.id}
-            onIncrement={() => this.handleIncrement(counter)}
-            onDelete={() => this.handleDelete(counter.id)}
+            onIncrement={() => this.props.onIncrement(counter)}
+            onDelete={() => this.props.onDelete(counter.id)}
             counter={counter}
           />
         ))}
