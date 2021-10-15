f420a8ed0a54b2ccf2190b09abfd4608da890316
Anas Abu Farraj <anas.r.abufarraj@gmail.com>
2021-10-15 15:08:20 +0300

    Stateless Functional Components

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,33 +1,64 @@
-import React, { Component } from 'react';
-
-class NavBar extends Component {
-  render() {
-    return (
-      <nav className="navbar navbar-dark bg-dark">
-        <div className="container-fluid">
-          <a className="navbar-brand" href="http://localhost:3000/">
-            MENU
-          </a>
-          <form className="d-flex">
-            <input
-              className="form-control me-2"
-              type="search"
-              placeholder="Search"
-              aria-label="Search"
-            />
-            <button className="btn btn-outline-light" type="submit">
-              Search
-            </button>
-          </form>
-          <span className="text-light">
-            <span className="badge bg-danger">
-              {this.props.items} {this.props.items === 1 ? 'Item' : 'Items'}
-            </span>
+// Stateless functional component
+function NavBar(props) {
+  return (
+    <nav className="navbar navbar-dark bg-dark">
+      <div className="container-fluid">
+        <a className="navbar-brand" href="http://localhost:3000/">
+          MENU
+        </a>
+        <form className="d-flex">
+          <input
+            className="form-control me-2"
+            type="search"
+            placeholder="Search"
+            aria-label="Search"
+          />
+          <button className="btn btn-outline-light" type="submit">
+            Search
+          </button>
+        </form>
+        <span className="text-light">
+          <span className="badge bg-danger">
+            {props.items} {props.items === 1 ? 'Item' : 'Items'}
           </span>
-        </div>
-      </nav>
-    );
-  }
+        </span>
+      </div>
+    </nav>
+  );
 }
 
 export default NavBar;
+
+// import React, { Component } from 'react';
+
+// class NavBar extends Component {
+//   render() {
+//     return (
+//       <nav className="navbar navbar-dark bg-dark">
+//         <div className="container-fluid">
+//           <a className="navbar-brand" href="http://localhost:3000/">
+//             MENU
+//           </a>
+//           <form className="d-flex">
+//             <input
+//               className="form-control me-2"
+//               type="search"
+//               placeholder="Search"
+//               aria-label="Search"
+//             />
+//             <button className="btn btn-outline-light" type="submit">
+//               Search
+//             </button>
+//           </form>
+//           <span className="text-light">
+//             <span className="badge bg-danger">
+//               {this.props.items} {this.props.items === 1 ? 'Item' : 'Items'}
+//             </span>
+//           </span>
+//         </div>
+//       </nav>
+//     );
+//   }
+// }
+//
+// export default NavBar;
