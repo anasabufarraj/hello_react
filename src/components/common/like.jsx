5d772655479284b742e42162620a702f05ef6a74
Anas Abu Farraj <anas.r.abufarraj@gmail.com>
2021-10-17 00:02:11 +0300

    Cleanup

--- src/components/common/like.jsx
+++ src/components/common/like.jsx
@@ -11,20 +11,3 @@ function Like(props) {
 }
 
 export default Like;
-
-// import React, { Component } from 'react';
-//
-// class Like extends Component {
-//   render() {
-//     let iconClass = this.props.liked ? 'fa-heart' : 'fa-heart-o';
-//     return (
-//       <i
-//         style={{ fontSize: 20, cursor: 'pointer' }}
-//         className={`fa ${iconClass} text-danger test`}
-//         onClick={this.props.onLike}
-//       />
-//     );
-//   }
-// }
-//
-// export default Like;
