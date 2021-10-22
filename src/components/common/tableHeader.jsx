481236f563952c564df3309a7897ecd8d6b804d9
Anas Abu Farraj <anas.r.abufarraj@gmail.com>
2021-10-22 23:21:24 +0300

    Sorting - Adding the Sort Icon...my solution

--- src/components/common/tableHeader.jsx
+++ src/components/common/tableHeader.jsx
@@ -13,13 +13,35 @@ class TableHeader extends Component {
     this.props.onSort(sortColumn);
   }
 
+  renderIcon(column) {
+    if (!column.content) {
+      if (this.props.sortColumn.path === column.path) {
+        return this.props.sortColumn.order === 'asc' ? (
+          <i className="fa fa-caret-down">&nbsp;</i>
+        ) : (
+          <i className="fa fa-caret-up">&nbsp;</i>
+        );
+      }
+    }
+  }
+
+  renderCursor(column) {
+    if (!column.content) {
+      return { cursor: 'pointer' };
+    }
+  }
+
   render() {
     return (
       <thead>
         <tr>
           {this.props.columns.map((column) => (
-            <th key={column.path} onClick={() => this.raiseSort(column.path)}>
-              {column.label}
+            <th
+              style={this.renderCursor(column)}
+              key={column.path}
+              onClick={() => this.raiseSort(column.path)}
+            >
+              {column.label} {this.renderIcon(column)}
             </th>
           ))}
         </tr>
