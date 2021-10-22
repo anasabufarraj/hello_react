5290520e4cabc6d2b51cc6f32e1a724b76117040
Anas Abu Farraj <anas.r.abufarraj@gmail.com>
2021-10-23 00:58:50 +0300

    Activate "All Genres" by default

--- src/components/common/ListGroup.jsx
+++ src/components/common/ListGroup.jsx
@@ -1,23 +1,33 @@
-function ListGroup(props) {
-  // MEMO: props are read-only arguments)
-  return (
-    <div className="list-group m-3">
-      {props.items.map((genre) => (
-        <li
-          style={{ cursor: 'pointer' }}
-          key={genre._id}
-          className={
-            genre === props.selectedItem
-              ? 'list-group-item list-group-item-action active'
-              : 'list-group-item list-group-item-action'
-          }
-          onClick={() => props.onItemSelect(genre)}
-        >
-          {genre.name}
-        </li>
-      ))}
-    </div>
-  );
+import { Component } from 'react';
+
+class ListGroup extends Component {
+  selectDefaultGenre(genre) {
+    if (!this.props.selectedItem && genre.name === 'All Genres') {
+      return 'list-group-item list-group-item-action active';
+    } else {
+      return genre === this.props.selectedItem
+        ? 'list-group-item list-group-item-action active'
+        : 'list-group-item list-group-item-action';
+    }
+  }
+
+  render() {
+    console.log(this.props.items.length);
+    return (
+      <div className="list-group m-3">
+        {this.props.items.map((genre) => (
+          <li
+            style={{ cursor: 'pointer' }}
+            key={genre._id}
+            className={this.selectDefaultGenre(genre)}
+            onClick={() => this.props.onItemSelect(genre)}
+          >
+            {genre.name}
+          </li>
+        ))}
+      </div>
+    );
+  }
 }
 
 export default ListGroup;
